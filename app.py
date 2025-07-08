43c13e5e022f5c7b8d6d16c44ff0465dcba89b56
M Dawood <aftabdawood36@email.com>
2025-07-08 19:04:54 +0500

    s

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = 'MAIL_USERNAME'
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
 mail = Mail(app)
 
 
